c18916d3526656e658470ac206840be0116a2bd4
mayuravaani <mmayuravaani@gmail.com>
2022-12-08 16:06:25 +0530

    min que added

---
+++ java/minimum.java
@@ -0,0 +1,9 @@
+/*
+Problem:
+John has to travel to another place. For this, he can book any one of two cab services.
+
+The first cab service charges X rupees.
+The second cab service charges Y rupees.
+Chef wants to spend the minimum amount of money. Which cab service should Chef take?
+
+*/
